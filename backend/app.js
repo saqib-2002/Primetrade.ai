2717282bbd7f691bed1088e7c52f178c1134869b
Saqib <saqibsaifi720@gmail.com>
2025-12-30 23:20:33 +0530

    connected mongodb to the server

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-
+connectDB();
 app.use(express(json));
 
 app.listen(PORT, () => {
